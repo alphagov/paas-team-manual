bc7853d98ba09b5bbf61c5e3fd569b2b98cfa325
Hector Rivas Gandara <keymon@gmail.com>
2018-04-30 11:01:15 +0100

    Inject google site search using js
    Inject the Google Site search just before the navigation title.
    
    This code inject on load the HTML element <gcse:search>[1] and
    loads the google JS to configure the search. The custom
    search engine has been configured using:
    https://cse.google.co.uk/cse/ with my GDS google account (hector).
    
    For any change, we can simply generate a new search in a different
    account.
    
    The search results will open as a 'popup' over the page.
    
    [1] https://developers.google.com/custom-search/docs/element

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,23 @@
 //= require govuk_tech_docs
+
+$(document).ready(function() {
+    /*
+    * Adds Element BEFORE NeighborElement
+    * Via: https://stackoverflow.com/a/32135318
+    */
+    Element.prototype.appendBefore = function(element) {
+    element.parentNode.insertBefore(this, element);
+    }, false;
+
+    var gcseSearchboxElement = document.createElement('div');
+    gcseSearchboxElement.innerHTML = '<gcse:search></gcse:search>';
+    gcseSearchboxElement.appendBefore(document.getElementById('toc'));
+
+    var cx = '017564251452351296425:wtwqadlvyxa';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+});
